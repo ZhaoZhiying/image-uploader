362e56814704f7987dca1900be28de537e0f0d14
ZhaoZhiYing <31733268+ZhaoZhiYing@users.noreply.github.com>
2018-03-21 17:18:00 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+upload.onchange = function(e){
+    let formData = new FormData()
+    formData.append('xxx', e.target.files[0])
+    $.ajax({
+        url: 'xxx',
+        data: formData,
+        processData: false,
+        contentType: false,
+        type: 'POST'
+    })
+}
